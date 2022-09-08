baac516fea72edeede2ff6ce26934e98f7e34f39
srr101 <songruirui1@huawei.com>
2022-09-08 11:02:59 +0800

    主干bugfix回合beta2版本
    Signed-off-by: srr101 <songruirui1@huawei.com>

--- data_share/frameworks/native/common/src/datashare_result_set.cpp
+++ data_share/frameworks/native/common/src/datashare_result_set.cpp
@@ -69,10 +69,14 @@ int DataShareResultSet::GetRowCount(int &count)
 
 bool DataShareResultSet::OnGo(int startRowIndex, int targetRowIndex)
 {
-    if (bridge_ == nullptr || blockWriter_ == nullptr) {
-        LOG_ERROR("bridge_ or blockWriter_ is null!");
+    if (bridge_ == nullptr || blockWriter_ == nullptr || sharedBlock_ == nullptr) {
+        LOG_ERROR("bridge_ or blockWriter_ or sharedBlock_ is null!");
         return E_ERROR;
     }
+    std::vector<std::string> columnNames;
+    GetAllColumnNames(columnNames);
+    sharedBlock_->Clear();
+    sharedBlock_->SetColumnNum(columnNames.size());
     return bridge_->OnGo(startRowIndex, targetRowIndex, *blockWriter_);
 }
 
